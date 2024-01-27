382b36e529ea3735231e00ff6f921189d12ce4d5
nguyenanhung <info@nguyenanhung.com>
2024-01-27 14:22:36 +0700

    Add new Service

---
+++ src/FunnyNews/FunnyNews.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Project bear-service
+ * Created by PhpStorm
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Copyright: 713uk13m <dev@nguyenanhung.com>
+ * Date: 27/01/2024
+ * Time: 14:21
+ */
+
+namespace Bear8421\Bear\Services\FunnyNews;
+
+use Bear8421\Bear\Services\Environment;
+use Bear8421\Bear\Services\Traits\Helper;
+use Bear8421\Bear\Services\Traits\Version;
+
+class FunnyNews implements Environment
+{
+    use Version, Helper;
+}
---
+++ src/Traits/Response.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Project bear-service
+ * Created by PhpStorm
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Copyright: 713uk13m <dev@nguyenanhung.com>
+ * Date: 27/01/2024
+ * Time: 14:18
+ */
+
+namespace Bear8421\Bear\Services\Traits;
+
+trait Response
+{
+    protected $response = array();
+
+    /**
+     * Function getResponse
+     *
+     * User: 713uk13m <dev@nguyenanhung.com>
+     * Copyright: 713uk13m <dev@nguyenanhung.com>
+     * @return array
+     */
+    public function getResponse(): array
+    {
+        return $this->response;
+    }
+}
