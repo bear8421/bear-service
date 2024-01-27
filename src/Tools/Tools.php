c15a4393d6688d041ac599204a088855bf27b5c2
nguyenanhung <info@nguyenanhung.com>
2024-01-27 15:22:50 +0700

    Add Tools: AccuWeather

---
+++ src/Tools/Tools.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Project bear-service
+ * Created by PhpStorm
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Copyright: 713uk13m <dev@nguyenanhung.com>
+ * Date: 27/01/2024
+ * Time: 14:58
+ */
+
+namespace Bear8421\Bear\Services\Tools;
+
+use Bear8421\Bear\Services\Environment;
+use Bear8421\Bear\Services\Traits\Helper;
+use Bear8421\Bear\Services\Traits\Version;
+
+class Tools implements Environment
+{
+    use Version, Helper;
+}
