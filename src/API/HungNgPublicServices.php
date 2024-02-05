7d42369c247181a119a0af88ff6e3dd6bfb765e7
nguyenanhung <info@nguyenanhung.com>
2024-02-06 05:36:49 +0700

    Add function: getCurrentIP

--- src/API/HungNgPublicServices.php
+++ src/API/HungNgPublicServices.php
@@ -24,76 +24,97 @@ use Bear8421\Bear\Services\Request\Request;
  */
 class HungNgPublicServices implements Environment
 {
-    use Version, Helper;
+	use Version, Helper;
 
-    const ENDPOINT = 'https://api.nguyenanhung.com';
+	const ENDPOINT = 'https://api.nguyenanhung.com';
 
-    private $toArrayResponse = false;
+	private $toArrayResponse = false;
 
-    public function toArray(): self
-    {
-        $this->toArrayResponse = true;
-        return $this;
-    }
+	public function toArray(): self
+	{
+		$this->toArrayResponse = true;
+		return $this;
+	}
 
-    /**
-     * Function getMe
-     *
-     * @return mixed|object|null
-     * @author   : 713uk13m <dev@nguyenanhung.com>
-     * @copyright: 713uk13m <dev@nguyenanhung.com>
-     * @time     : 31/07/2023 14:39
-     */
-    public function getMe()
-    {
-        $request = new Request();
-        $api = self::ENDPOINT . '/me';
-        $json = $request->sendRequest($api);
-        if ($this->isJson($json)) {
-            if ($this->toArrayResponse === true) {
-                return json_decode($json, true);
-            }
-            return json_decode($json, false);
-        }
-        return null;
-    }
+	/**
+	 * Function getMe
+	 *
+	 * @return mixed|object|null
+	 * @author   : 713uk13m <dev@nguyenanhung.com>
+	 * @copyright: 713uk13m <dev@nguyenanhung.com>
+	 * @time     : 31/07/2023 14:39
+	 */
+	public function getMe()
+	{
+		$request = new Request();
+		$api = self::ENDPOINT . '/me';
+		$json = $request->sendRequest($api);
+		if ($this->isJson($json)) {
+			if ($this->toArrayResponse === true) {
+				return json_decode($json, true);
+			}
+			return json_decode($json, false);
+		}
+		return null;
+	}
 
-    /**
-     * Function getInfoOfMe
-     *
-     * @return mixed|object|null
-     * @author   : 713uk13m <dev@nguyenanhung.com>
-     * @copyright: 713uk13m <dev@nguyenanhung.com>
-     * @time     : 31/07/2023 14:56
-     */
-    public function getInfoOfMe()
-    {
-        $me = $this->getMe();
-        if ($this->toArrayResponse === true) {
-            return $me['info'] ?? null;
-        }
-        return $me->info ?? null;
-    }
+	/**
+	 * Function getCurrentIP
+	 *
+	 * User: 713uk13m <dev@nguyenanhung.com>
+	 * Copyright: 713uk13m <dev@nguyenanhung.com>
+	 * @return mixed|null
+	 */
+	public function getCurrentIP()
+	{
+		$request = new Request();
+		$api = self::ENDPOINT . '/ip';
+		$json = $request->sendRequest($api);
+		if ($this->isJson($json)) {
+			if ($this->toArrayResponse === true) {
+				return json_decode($json, true);
+			}
+			return json_decode($json, false);
+		}
+		return null;
+	}
 
-    /**
-     * Function getProfileOfMe
-     *
-     * @return mixed|null
-     * @author   : 713uk13m <dev@nguyenanhung.com>
-     * @copyright: 713uk13m <dev@nguyenanhung.com>
-     * @time     : 31/10/2023 16:35
-     */
-    public function getProfileOfMe()
-    {
-        $request = new Request();
-        $api = self::ENDPOINT . '/me/profile';
-        $json = $request->sendRequest($api);
-        if ($this->isJson($json)) {
-            if ($this->toArrayResponse === true) {
-                return json_decode($json, true);
-            }
-            return json_decode($json, false);
-        }
-        return null;
-    }
+	/**
+	 * Function getInfoOfMe
+	 *
+	 * @return mixed|object|null
+	 * @author   : 713uk13m <dev@nguyenanhung.com>
+	 * @copyright: 713uk13m <dev@nguyenanhung.com>
+	 * @time     : 31/07/2023 14:56
+	 */
+	public function getInfoOfMe()
+	{
+		$me = $this->getMe();
+		if ($this->toArrayResponse === true) {
+			return $me['info'] ?? null;
+		}
+		return $me->info ?? null;
+	}
+
+	/**
+	 * Function getProfileOfMe
+	 *
+	 * @return mixed|null
+	 * @author   : 713uk13m <dev@nguyenanhung.com>
+	 * @copyright: 713uk13m <dev@nguyenanhung.com>
+	 * @time     : 31/10/2023 16:35
+	 */
+	public function getProfileOfMe()
+	{
+		$request = new Request();
+		$api = self::ENDPOINT . '/me/profile';
+		$json = $request->sendRequest($api);
+		if ($this->isJson($json)) {
+			if ($this->toArrayResponse === true) {
+				return json_decode($json, true);
+			}
+			return json_decode($json, false);
+		}
+		return null;
+	}
 }
