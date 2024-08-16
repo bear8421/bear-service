e14de1459a52886b2a649ab4d614db6d08f2dae3
HungNA - Technical Manager <hungan@lisod.vn>
2024-08-16 13:13:57 +0700

    Version 1.2.1 - Update admin helpers

--- helpers/admin.php
+++ helpers/admin.php
@@ -8,22 +8,3 @@ if (!function_exists('apply_url_link')) {
         return trim($str);
     }
 }
-if (!function_exists('generateHtpasswdPassword')) {
-    function generateHtpasswdPassword($plainTextPassword): string
-    {
-        // Tạo salt ngẫu nhiên cho bcrypt (2y) hoặc MD5 ($1$)
-        $cost = 12;
-        $salt = substr(sha1(date('YmdHisu') . generate_uuid_v4()), 0, 22);
-
-        // Mã hóa mật khẩu với bcrypt
-        $hashedPassword = crypt($plainTextPassword, '$2y$' . $cost . '$' . $salt . '$');
-
-        // Kiểm tra nếu hệ thống hỗ trợ bcrypt
-        if (strlen($hashedPassword) < 60) {
-            // Nếu không hỗ trợ bcrypt, fallback to MD5
-            $hashedPassword = crypt($plainTextPassword, '$1$' . $salt . '$');
-        }
-
-        return $hashedPassword;
-    }
-}
--- helpers/helpers.php
+++ helpers/helpers.php
@@ -8,36 +8,7 @@
  * Date: 09/16/2021
  * Time: 18:00
  */
-if (!function_exists('generate_uuid_v4')) {
-    /**
-     * Function generate_uuid_v4
-     *
-     * @return string
-     * @author   : 713uk13m <dev@nguyenanhung.com>
-     * @copyright: 713uk13m <dev@nguyenanhung.com>
-     * @time     : 09/11/2021 52:37
-     */
-    function generate_uuid_v4()
-    {
-        return sprintf(
-            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            mt_rand(0, 0xffff), // 32 bits for "time_low"
-            mt_rand(0, 0xffff), // 16 bits for "time_mid"
-            mt_rand(0, 0xffff), // 16 bits for "time_hi_and_version",
 
-            // four most significant bits holds version number 4
-            mt_rand(0, 0x0fff) | 0x4000, // 16 bits, 8 bits for "clk_seq_hi_res",
-
-            // 8 bits for "clk_seq_low",
-
-            // two most significant bits holds zero and one for variant DCE1.1
-            mt_rand(0, 0x3fff) | 0x8000, // 48 bits for "node"
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff)
-        );
-    }
-}
 // ~~~~~ Blogspot
 if (!function_exists('blogspotDescSort')) {
     function blogspotDescSort($item1, $item2): int
