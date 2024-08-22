af09aafeedafc97fb95aab2ac0dd07984610b26e
HungNA - Technical Manager <hungan@lisod.vn>
2024-08-22 14:29:47 +0700

    Update code

--- helpers/author.php
+++ helpers/author.php
@@ -1,19 +1,13 @@
 <?php
 
-use Bear8421\Bear\Services\Environment;
+use Bear8421\Bear\Services\Config\Author;
 
 if (!function_exists('powered_by_author')) {
     function powered_by_author($startYear = ''): string
     {
-        if (empty($startYear)) {
-            $copyYear = date('Y');
-        } else {
-            $copyYear = $startYear . '-' . date('Y');
-        }
-
-        $str = '&copy; ' . $copyYear;
-        $str .= ' ' . Environment::POWERED_BY_NAME;
-        $str .= ' (' . Environment::POWERED_BY_EMAIL . ')';
+        $str = '&copy; ' . Author::getCopyrightYear($startYear);
+        $str .= ' ' . Author::getAuthorName();
+        $str .= ' (' . Author::getAuthorEmail() . ')';
         $str .= '. All Rights Reserved.';
         return trim($str);
     }
--- src/Config/Author.php
+++ src/Config/Author.php
@@ -8,4 +8,24 @@ use Bear8421\Bear\Services\Traits\Version;
 class Author implements Environment
 {
     use Version;
+
+    public static function getAuthorName(): string
+    {
+        return self::POWERED_BY_NAME;
+    }
+
+    public static function getAuthorEmail(): string
+    {
+        return self::POWERED_BY_EMAIL;
+    }
+
+    public static function getCopyrightYear($startYear = '')
+    {
+        if (empty($startYear)) {
+            $copyrightYear = date('Y');
+        } else {
+            $copyrightYear = $startYear . '-' . date('Y');
+        }
+        return $copyrightYear;
+    }
 }
