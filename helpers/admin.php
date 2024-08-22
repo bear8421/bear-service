5da82ecdec1f2fd9b2d7768b97042897ab3460dc
HungNA - Technical Manager <hungan@lisod.vn>
2024-08-22 14:09:50 +0700

    Update code

--- helpers/admin.php
+++ helpers/admin.php
@@ -4,7 +4,27 @@ if (!function_exists('apply_url_link')) {
     function apply_url_link($name = '', $link = ''): string
     {
         $str = trim($name) . ': ';
-        $str .= '<a href="' . trim($link) . '" target="_blank">' . trim($link) . '</a>';
+        $title = 'title="Click here to tools: ' . trim($name) . '"';
+        $str .= '<a ' . $title . ' href="' . trim($link) . '" target="_blank">' . trim($link) . '</a>';
         return trim($str);
+    }
+}
+
+if (!function_exists('admin_apply_url_link')) {
+    function admin_apply_url_link($name = '', $link = ''): string
+    {
+        return '<li>' . apply_url_link($name, $link) . '</li>';
+    }
+}
+
+if (!function_exists('admin_apply_url_link_required_ext')) {
+    function admin_apply_url_link_required_ext($name = '', $link = '', $ext = ''): string
+    {
+        if (empty($ext)) {
+            return '';
+        }
+        if (extension_loaded($ext)) {
+            return admin_apply_url_link($name, $link);
+        }
     }
 }
